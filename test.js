22e730d88f7cca55bf75c4d37b578c4a4e3b5b09
Jack Danzey <jackdanzey@gmail.com>
2022-07-25 20:44:27 -0400

    practice

--- test.js
+++ test.js
@@ -1136,13 +1136,48 @@
 // 1. Start with an array of numbers and create a new array with only the numbers less than 20.
 // #     For example, [2, 32, 80, 18, 12, 3] becomes [2, 18, 12, 3].
 
+// let array = [2, 3, 22, 10];
+// let newArray = [];
 
+// array.forEach(function (num) {
+//   if (num < 20) {
+//     newArray.push(num);
+//   }
+// });
+
+// console.log(newArray);
 
 // #  2. Start with an array of strings and create a new array with only the strings that start with the letter "w".
 // #     For example, ["winner", "winner", "chicken", "dinner"] becomes ["winner", "winner"].
 
+// let array = ["winner", "winner", "chicken", "dinner"];
+// let newArray = [];
+
+// for (let index = 0; index < array.length; index++) {
+//   if (array[index][0] === "w") {
+//     newArray.push(array[index]);
+//   }
+// }
+
+// console.log(newArray);
+
 // #  3. Start with an array of hashes and create a new array with only the hashes with prices greater than 5 (from the :price key).
 // #     For example, [{name: "chair", price: 100}, {name: "pencil", price: 1}, {name: "book", price: 4}] becomes [{name: "chair", price: 100}].
+
+// let array = [
+//   { name: "chair", price: 100 },
+//   { name: "pencil", price: 1 },
+//   { name: "book", price: 4 },
+// ];
+// let newArray = [];
+
+// array.forEach(function (hash) {
+//   if (hash.price > 5) {
+//     newArray.push(hash);
+//   }
+// });
+
+// console.log(newArray);
 
 // #  4. Start with an array of numbers and create a new array with only the even numbers.
 // #     For example, [2, 4, 5, 1, 8, 9, 7] becomes [2, 4, 8].
@@ -1158,3 +1193,22 @@
 
 // #  8. Start with an array of strings and create a new array with only the strings that don't start with the letter "b".
 // #     For example, ["big", "little", "good", "bad"] becomes ["little", "good"].
+
+// Given a string, write a function that returns true if the “$” character is contained within the string or false if it is not.
+
+// Input: “i hate $ but i love money i know i know im crazy”
+// Output: true
+
+// Input: “abcdefghijklmnopqrstuvwxyz”
+// Output: false
+
+// let string = "I hate but i love money i know i know im crazy";
+// let $indicator = false;
+
+// for (let index = 0; index < string.length; index++) {
+//   if (string[index] === "$") {
+//     $indicator = true;
+//   }
+// }
+
+// console.log($indicator);
