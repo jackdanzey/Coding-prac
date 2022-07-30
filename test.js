186bf4848cb01d75df12f31bc9ebcff09c6295d6
Jack Danzey <jackdanzey@gmail.com>
2022-07-30 09:19:39 -0400

    completed a couple of problems

--- test.js
+++ test.js
@@ -1400,3 +1400,50 @@
 // }
 
 // console.log(hash);
+
+// Given a hash, where the keys are strings representing food items, and the values are numbers representing the price of each food, return the amount someone would pay if they'd order one of each food from the entire menu.
+
+// Input: {"hot dog" => 2, "hamburger" => 3, "steak sandwich" => 5, "fries" => 1, "cole slaw" => 1, "soda" => 2}
+
+// Output: 14
+
+// Explanation: If someone would order one of everything on the menu, they'd pay a total of 14 (the sum of all the hash's values).
+
+// let hash = { "hot dog": 2, hamburger: 3, "steak sandwich": 5, fries: 1, "cole slaw": 1, soda: 2 };
+// let sum = 0;
+// Object.entries(hash).forEach(function ([key, value]) {
+//   sum = sum + value;
+// });
+
+// console.log(sum);
+
+// Given an array of hashes that represent a list of social media posts, return a new array that only contains the posts that have at least 1000 likes.
+
+// Input: [
+// {title: 'Me Eating Pizza', submitted_by: "Joelle P.", likes: 1549},
+// {title: 'i never knew how cool i was until now', submitted_by: "Lyndon Johnson", likes: 3},
+// {title: 'best selfie evar!!!', submitted_by: "Patti Q.", likes: 1092},
+// {title: 'Mondays are the worst', submitted_by: "Aunty Em", likes: 644}
+// ]
+
+// Output: [
+// {title: 'Me Eating Pizza', submitted_by: "Joelle P.", likes: 1549},
+// {title: 'best selfie evar!!!', submitted_by: "Patti Q.", likes: 1092},
+// ]
+
+// let array = [
+//   { title: "Me Eating Pizza", submitted_by: "Joelle P.", likes: 1549 },
+//   { title: "i never knew how cool i was until now", submitted_by: "Lyndon Johnson", likes: 3 },
+//   { title: "best selfie evar!!!", submitted_by: "Patti Q.", likes: 1092 },
+//   { title: "Mondays are the worst", submitted_by: "Aunty Em", likes: 644 },
+// ];
+
+// let output = [];
+
+// array.forEach(function (element) {
+//   if (element.likes > 1000) {
+//     output.push(element);
+//   }
+// });
+
+// console.log(output);
