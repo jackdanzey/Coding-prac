5c8677bc2403d804c20f8e4f6ffe26c273fdaa03
Jack Danzey <jackdanzey@gmail.com>
2022-08-07 08:37:43 -0400

    did some algorithm problems

--- test.js
+++ test.js
@@ -1667,3 +1667,75 @@
 // });
 
 // console.log(highest);
+
+// Given a hash, where the keys are strings representing food items, and the values are numbers representing the price of each food, return the amount someone would pay if they'd order one of each food from the entire menu.
+
+// Input: {"hot dog" => 2, "hamburger" => 3, "steak sandwich" => 5, "fries" => 1, "cole slaw" => 1, "soda" => 2}
+
+// Output: 14
+
+// Explanation: If someone would order one of everything on the menu, they'd pay a total of 14 (the sum of all the hash's values).
+
+// let hash = { "hot dog": 2, hamburger: 3, "steak sandwich": 5, fries: 1, "cole slaw": 1, soda: 2 };
+// let total = 0;
+
+// Object.values(hash).forEach(function (value) {
+//   total = total + value;
+// });
+
+// console.log(total);
+
+// Given an array of social media posts and an array of users, return a list of posts (as an array of hashes) that replaces the submitted_by id number as the person's actual name.
+
+// For example, given this array of posts (note that the submitted_by is an id number):
+
+// [
+// {title: 'Me Eating Pizza', submitted_by: 231, likes: 1549},
+// {title: 'i never knew how cool i was until now', submitted_by: 989, likes: 3},
+// {title: 'best selfie evar!!!', submitted_by: 111, likes: 1092},
+// {title: 'Mondays are the worst', submitted_by: 403, likes: 644}
+// ]
+
+// And this array of users:
+
+// [
+// {user_id: 403, name: "Aunty Em"},
+// {user_id: 231, name: "Joelle P."},
+// {user_id: 989, name: "Lyndon Johnson"},
+// {user_id: 111, name: "Patti Q."},
+// ]
+
+// Return the array of posts as follows:
+
+// [
+// {title: 'Me Eating Pizza', submitted_by: "Joelle P.", likes: 1549},
+// {title: 'i never knew how cool i was until now', submitted_by: "Lyndon Johnson", likes: 3},
+// {title: 'best selfie evar!!!', submitted_by: "Patti Q.", likes: 1092},
+// {title: 'Mondays are the worst', submitted_by: "Aunty Em", likes: 644}
+// ]
+
+// let posts = [
+//   { title: "Me Eating Pizza", submitted_by: 231, likes: 1549 },
+//   { title: "i never knew how cool i was until now", submitted_by: 989, likes: 3 },
+//   { title: "best selfie evar!!!", submitted_by: 111, likes: 1092 },
+//   { title: "Mondays are the worst", submitted_by: 403, likes: 644 },
+// ];
+// let users = [
+//   { user_id: 403, name: "Aunty Em" },
+//   { user_id: 231, name: "Joelle P." },
+//   { user_id: 989, name: "Lyndon Johnson" },
+//   { user_id: 111, name: "Patti Q." },
+// ];
+
+// let newPosts = [];
+// let index = 1;
+// let otherIndex = 0;
+
+// Object.entries(posts).forEach(function ([key, value]) {
+//   newPosts.push(key, value);
+//   newPosts[index].submitted_by = users[otherIndex].name;
+//   index += 2;
+//   otherIndex += 1;
+// });
+
+// console.log(newPosts);
