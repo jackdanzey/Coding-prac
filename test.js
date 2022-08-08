ccaae511d63b7ce71aee8187fa9e6fcd54c74260
Jack Danzey <jackdanzey@gmail.com>
2022-08-08 08:23:18 -0400

    did a hash problem

--- test.js
+++ test.js
@@ -1739,3 +1739,24 @@
 // });
 
 // console.log(newPosts);
+
+// Given an array of strings, return a hash that provides of a count of how many times each string occurs.
+
+// Input: ["Dewey", "Truman", "Dewey", "Dewey", "Truman", "Truman", "Dewey", "Truman", "Truman", "Dewey", "Dewey"]
+
+// Output: {"Dewey" => 6, "Truman" => 5}
+
+// Explanation: "Dewey" occurs 6 times in the array, while "Truman" occurs 5 times.
+
+// let array = ["Dewey", "Truman", "Dewey", "Dewey", "Truman", "Truman", "Dewey", "Truman", "Truman", "Dewey", "Dewey"];
+// let hash = {};
+
+// array.forEach(function (word) {
+//   if (!hash[word]) {
+//     hash[word] = 1;
+//   } else {
+//     hash[word] += 1;
+//   }
+// });
+
+// console.log(hash);
