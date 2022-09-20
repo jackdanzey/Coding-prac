bf736ca6f5fadf83f27419af957f60151f115039
Jack Danzey <jackdanzey@gmail.com>
2022-09-20 08:17:46 -0400

    first commit

--- prac.py
+++ prac.py
@@ -1,5 +1,5 @@
 # Create a calculator which handles +,-,*,/ and outputs answer based on the mode/ operator used
-# Hint: use 3 separate inputs 
+# Hint: use 3 separate inputs
 # Bonus: Extend functionality with extra mode so it also does celsius to fahrenheit conversion
 # formula is: temp in C*9/5 + 32 = temp in f
 
@@ -21,3 +21,19 @@
 #     else:
 #         print('Input error!')
 
+# num = int(input('Enter a number to find out if it is either even or odd: '))
+
+# if num % 2 > 0:
+#     print('your number is odd')
+# else:
+#     print('your number is even')
+
+name = input('Please create a login name: ')
+password = input('Please create a login password: ')
+loginname = input('Please enter your login name: ')
+loginpassword = input('Please enter your login password: ')
+
+if loginname != name or loginpassword != password:
+    print('invalid credentials, dumbass!')
+else:
+    print('login successful!')
