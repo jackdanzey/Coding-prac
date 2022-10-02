89651d9933748305ea5dbb2db5a9b3604766f5da
Jack Danzey <jackdanzey@gmail.com>
2022-10-02 09:05:34 -0400

    did a hackerrank problem

--- prac.py
+++ prac.py
@@ -9,40 +9,57 @@
 # Ver 1.5 print inventory before and after purchases as one department_store of stuff(combine inventories from all stores into one...pretend Big Biz bought all the local stores, and want constant reporting for inventory management...)
 # as in all games there is a special way to do this that actually makes money and solves the problem...can you find 'them'? Do you know why? May require knowledge of actual python 'lore'
 
-freelancers = {'name': 'freelancing Shop', 'brian': 70, 'black knight': 20,
-               'biccus diccus': 100, 'grim reaper': 500, 'minstrel': -15}
-antiques = {'name': 'Antique Shop', 'french castle': 400,
-            'wooden grail': 3, 'scythe': 150, 'catapult': 75, 'german joke': 5}
-pet_shop = {'name': 'Pet Shop', 'blue parrot': 10,
-            'white rabbit': 5, 'newt': 2}
-department_store = {}
+# freelancers = {'name': 'freelancing Shop', 'brian': 70, 'black knight': 20,
+#                'biccus diccus': 100, 'grim reaper': 500, 'minstrel': -15}
+# antiques = {'name': 'Antique Shop', 'french castle': 400,
+#             'wooden grail': 3, 'scythe': 150, 'catapult': 75, 'german joke': 5}
+# pet_shop = {'name': 'Pet Shop', 'blue parrot': 10,
+#             'white rabbit': 5, 'newt': 2}
+# department_store = {}
 
-for department in (freelancers, antiques, pet_shop):
-    department_store.update(department)
+# for department in (freelancers, antiques, pet_shop):
+#     department_store.update(department)
 
-department_store.pop('name')
-print('Morning inventory of stores', sorted(department_store.items()))
-print('-----------------')
-cart = {}
-purse = 1000
-buy_items1 = ''
+# department_store.pop('name')
+# print('Morning inventory of stores', sorted(department_store.items()))
+# print('-----------------')
+# cart = {}
+# purse = 1000
+# buy_items1 = ''
 
-for shop in (freelancers, antiques, pet_shop):
-    buy_item = input(
-        f'Welcome to {shop["name"]}! (type exit to exit store) what do you want to buy: {shop}').lower()
-    if buy_item == 'exit':
-        print('exiting shop...')
-        continue
-    if buy_item not in shop:
-        print('This item does not exist! Moving on...')
-        continue
-    buy_items1 = buy_items1 + f'{buy_item}:{shop[buy_item]} Gp, '
-    cart.update({buy_item: shop.pop(buy_item)})
-    total_sum = sum(cart.values())
+# for shop in (freelancers, antiques, pet_shop):
+#     buy_item = input(
+#         f'Welcome to {shop["name"]}! (type exit to exit store) what do you want to buy: {shop}').lower()
+#     if buy_item == 'exit':
+#         print('exiting shop...')
+#         continue
+#     if buy_item not in shop:
+#         print('This item does not exist! Moving on...')
+#         continue
+#     buy_items1 = buy_items1 + f'{buy_item}:{shop[buy_item]} Gp, '
+#     cart.update({buy_item: shop.pop(buy_item)})
+#     total_sum = sum(cart.values())
 
-print(f'You Purchased {buy_items1}. Your total is {total_sum} Gp. Your purse now has {purse - total_sum} Gp. Have a nice day of mayhem!')
+# print(f'You Purchased {buy_items1}. Your total is {total_sum} Gp. Your purse now has {purse - total_sum} Gp. Have a nice day of mayhem!')
 
-department_store_after = {**freelancers, **antiques, **pet_shop}
-department_store_after.pop('name')
-print('-----------------')
-print('Evening inventory of stores', sorted(department_store_after.items()))
+# department_store_after = {**freelancers, **antiques, **pet_shop}
+# department_store_after.pop('name')
+# print('-----------------')
+# print('Evening inventory of stores', sorted(department_store_after.items()))
+
+# Given a square matrix, calculate the absolute difference between the sums of its diagonals.
+
+# def diagonalDifference(arr):
+#     index = 0
+#     first = 0
+#     second = 0
+#     inner = len(arr) - 1
+#     while index < len(arr):
+#         first = first + arr[index][index]
+#         second = second + arr[index][inner]
+#         inner -= 1
+#         index += 1
+#     return abs(first - second)
+
+
+# print(diagonalDifference([[1, 2, 3], [3, 4, 5], [6, 7, 8]]))
