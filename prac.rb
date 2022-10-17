88c9a57ef8027b16d35027bf50bf6a502ddf1b53
Jack Danzey <jackdanzey@gmail.com>
2022-10-17 08:31:55 -0400

    keepin sharp

--- prac.rb
+++ prac.rb
@@ -349,3 +349,16 @@
 #   end
 #   return s[0..7]
 # end
+
+# Given a hash, return a flat array containing all the hash’s keys and values.
+
+# Input: {“a” => 1, “b” => 2, “c” => 3, “d” => 4}
+# Output: [“a”, 1, “b”, 2, “c”, 3, “d”, 4]
+
+# hash = { "a" => 1, "b" => 2, "c" => 3, "d" => 4 }
+# array = []
+# hash.each do |key, value|
+#   array << key
+#   array << value
+# end
+# p array
