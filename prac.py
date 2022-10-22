2f6fa2fa4965917ac9fc1c8925d25e540702d1b7
Jack Danzey <jackdanzey@gmail.com>
2022-10-22 08:44:12 -0400

    little python prac

--- prac.py
+++ prac.py
@@ -96,3 +96,26 @@
 #             else:
 #                 numdict[x] = 1
 #         return min(numdict, key=numdict.get)
+
+# Given two strings, return true if they are anagrams of each other, and false if they are not. An anagram is a word, phrase, or name formed by rearranging the letters of another, such as cinema, formed from iceman.
+
+# Do not use any built-in sort methods.
+
+# Input: “silent”, “listen”
+# Output: true
+
+# Input: “frog”, “bear”
+# Output: false
+
+# inputone = 'iceman'
+# inputtwo = 'cinema'
+# count = 0
+# output = False
+# for letter in inputone:
+#     for inner in inputtwo:
+#         if letter == inner:
+#             count += 1
+#             if count == len(inputone):
+#                 output = True
+
+# print(output)
