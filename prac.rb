78f9e5056d3ddfebc0a35649a2573c787cdd6f69
Jack Danzey <jackdanzey@gmail.com>
2022-12-09 08:57:15 -0500

    bit of ruby...

--- prac.rb
+++ prac.rb
@@ -583,3 +583,20 @@
 # end
 
 # breaking_records([12, 24, 10, 24])
+
+# Given a string, write a function that returns a copy of the original string that has every other character capitalized. (Capitalization should begin with the second character.)
+
+# Input: “hello, how are your porcupines today?”
+# Output: “hElLo, HoW ArE YoUr pOrCuPiNeS ToDaY?”
+
+# input = "elden ring goty woooooooo!"
+# index = 0
+
+# while index < input.length
+#   if index % 2 != 0
+#     input[index] = input[index].upcase
+#   end
+#   index += 1
+# end
+
+# puts input
