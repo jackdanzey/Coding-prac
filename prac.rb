65d6dc53042da65fa933c1cd4d63739e75a81794
Jack Danzey <jackdanzey@gmail.com>
2022-12-30 08:28:16 -0500

    little ruby problem

--- prac.rb
+++ prac.rb
@@ -642,3 +642,36 @@
 # end
 
 # p combo
+
+# Given an array of hashes that represent a list of social media posts, return a new array that only contains the posts that have at least 1000 likes.
+
+# Input: [
+# {title: 'Me Eating Pizza', submitted_by: "Joelle P.", likes: 1549},
+# {title: 'i never knew how cool i was until now', submitted_by: "Lyndon Johnson", likes: 3},
+# {title: 'best selfie evar!!!', submitted_by: "Patti Q.", likes: 1092},
+# {title: 'Mondays are the worst', submitted_by: "Aunty Em", likes: 644}
+# ]
+
+# Output: [
+# {title: 'Me Eating Pizza', submitted_by: "Joelle P.", likes: 1549},
+# {title: 'best selfie evar!!!', submitted_by: "Patti Q.", likes: 1092},
+# ]
+
+# posts = [
+#   { title: "Me Eating Pizza", submitted_by: "Joelle P.", likes: 1549 },
+#   { title: "i never knew how cool i was until now", submitted_by: "Lyndon Johnson", likes: 3 },
+#   { title: "best selfie evar!!!", submitted_by: "Patti Q.", likes: 1092 },
+#   { title: "Mondays are the worst", submitted_by: "Aunty Em", likes: 644 },
+# ]
+
+# liked_posts = []
+# index = 0
+
+# while index < posts.length
+#   if posts[index][:likes] > 1000
+#     liked_posts << posts[index]
+#   end
+#   index += 1
+# end
+
+# p liked_posts
