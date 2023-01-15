752eb8919756ce213fc2c8c83596ca21e4a8d7d0
Jack Danzey <jackdanzey@gmail.com>
2023-01-15 08:29:10 -0500

    translation problem done in python and javascript

--- prac.py
+++ prac.py
@@ -577,3 +577,28 @@
 
 # common(['flower', 'flow', 'flight'])
 # common(['dog', 'racecar', 'car'])
+
+# Given a DNA strand, return its RNA complement (per RNA transcription).
+
+# Both DNA and RNA strands are a sequence of nucleotides. Here we're representing the sequences with single-letter characters (e.g. a sample strand may look like: "AGCA".)
+
+# Given a string representing a DNA strand, provide its transcribed RNA strand, according to the following pattern:
+
+# G becomes C
+# C becomes G
+# T becomes A
+# A becomes U
+
+# So based on all this, here's a sample input/output:
+
+# Input: 'ACGTGGTCTTAA'
+# Output: 'UGCACCAGAAUU'
+
+# input = 'ACGTGGTCTTAA'
+# translator = {'G': 'C', 'C': 'G', 'T': 'A', 'A': 'U'}
+# output = ''
+
+# for x in input:
+#     output = output + translator[x]
+
+# print(output)
--- test.js
+++ test.js
@@ -2264,3 +2264,29 @@
 // });
 
 // console.log(newdic);
+
+// Given a DNA strand, return its RNA complement (per RNA transcription).
+
+// Both DNA and RNA strands are a sequence of nucleotides. Here we're representing the sequences with single-letter characters (e.g. a sample strand may look like: "AGCA".)
+
+// Given a string representing a DNA strand, provide its transcribed RNA strand, according to the following pattern:
+
+// G becomes C
+// C becomes G
+// T becomes A
+// A becomes U
+
+// So based on all this, here's a sample input/output:
+
+// Input: 'ACGTGGTCTTAA'
+// Output: 'UGCACCAGAAUU'
+
+// let dna = "ACGTGGTCTTAA";
+// let translator = { G: "C", C: "G", T: "A", A: "U" };
+// let output = "";
+
+// for (let index = 0; index < dna.length; index++) {
+//   output = output + translator[dna[index]];
+// }
+
+// console.log(output);
