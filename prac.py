1d5c8f5799cae81dd19921f919b119df31639a7a
Jack Danzey <jackdanzey@gmail.com>
2023-01-27 08:55:37 -0500

    simple dictionary reading problems in ruby and python

--- prac.py
+++ prac.py
@@ -645,3 +645,20 @@
 
 
 # selector([2, 1, 3, 2, 5, 1, 2, 6, 2, 7, 1, 5, 6, 3, 2, 6, 2, 1, 2])
+
+# Given a hash, where the keys are strings representing food items, and the values are numbers representing the price of each food, return the amount someone would pay if they'd order one of each food from the entire menu.
+
+# Input: {"hot dog" => 2, "hamburger" => 3, "steak sandwich" => 5, "fries" => 1, "cole slaw" => 1, "soda" => 2}
+
+# Output: 14
+
+# Explanation: If someone would order one of everything on the menu, they'd pay a total of 14 (the sum of all the hash's values).
+
+# dic = {"hot dog": 2, "hamburger": 3, "steak sandwich": 5,
+#        "fries": 1, "cole slaw": 1, "soda": 2}
+# cost = 0
+
+# for k, v in dic.items():
+#     cost = cost + v
+
+# print(cost)
--- prac.rb
+++ prac.rb
@@ -777,3 +777,20 @@
 #     { "title" => "best selfie evar!!!", "submitted_by" => "Patti Q.", "likes" => 1092 },
 #     { "title" => "Mondays are the worst", "submitted_by" => "Aunty Em", "likes" => 644 },
 #   ])
+
+# Given a hash, where the keys are strings representing food items, and the values are numbers representing the price of each food, return the amount someone would pay if they'd order one of each food from the entire menu.
+
+# Input: {"hot dog" => 2, "hamburger" => 3, "steak sandwich" => 5, "fries" => 1, "cole slaw" => 1, "soda" => 2}
+
+# Output: 14
+
+# Explanation: If someone would order one of everything on the menu, they'd pay a total of 14 (the sum of all the hash's values).
+
+# hash = { "hot dog" => 2, "hamburger" => 3, "steak sandwich" => 5, "fries" => 1, "cole slaw" => 1, "soda" => 2 }
+# cost = 0
+
+# hash.each do |key, value|
+#   cost = cost + value
+# end
+
+# p cost
