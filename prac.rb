c2b99f8a4a3a095ef5bcdec292cec2a07cb12fb9
Jack Danzey <jackdanzey@gmail.com>
2023-02-13 08:01:00 -0500

    Coolio problem in ruby

--- prac.rb
+++ prac.rb
@@ -860,3 +860,38 @@
 # end
 
 # vote_counter(votes)
+
+# Given an array of numbers, return true if the array is a "100 Coolio Array", or false if it is not.
+
+# A "100 Coolio Array" meets the following criteria:
+
+# Its first and last numbers add up to 100,
+# Its second and second-to-last numbers add up to 100,
+# Its third and third-to-last numbers add up to 100,
+# and so on and so forth.
+
+# Here are examples of 100 Coolio Arrays:
+
+# [1, 2, 3, 97, 98, 99]
+# [90, 20, 70, 100, 30, 80, 10]
+
+# def coolio(arr)
+#   index = 0
+#   inner = arr.length - 1
+#   result = "False"
+#   while index <= inner
+#     if arr[index] + arr[inner] == 100
+#       index += 1
+#       inner -= 1
+#     elsif index == inner && arr[index] == 100
+#       result = "100 Coolio Array"
+#       return result
+#     else
+#       return result
+#     end
+#   end
+#   result = "100 Coolio Array"
+#   return result
+# end
+
+# p coolio([90, 20, 70, 100, 30, 80, 10])
