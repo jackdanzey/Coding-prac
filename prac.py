602dcfbd027fc7045ac21d618a505b91f0ea4af7
Jack Danzey <jackdanzey@gmail.com>
2023-02-15 08:50:50 -0500

    bit of inheritance prac

--- prac.py
+++ prac.py
@@ -711,3 +711,29 @@
 #     newArray = False
 
 # print(newArray)
+
+# class Person:
+#     def __init__(self, fname, lname):
+#         self.firstname = fname
+#         self.lastname = lname
+
+#     def printname(self):
+#         print(self.firstname, self.lastname)
+
+
+# test = Person('Tom', 'Jones')
+# test.printname()
+
+
+# class Student(Person):
+#     def __init__(self, fname, lname, year):
+#         super().__init__(fname, lname)
+#         self.graduationyear = year
+
+#     def printstudent(self):
+#         print(self.firstname, self.lastname, self.graduationyear)
+
+
+# newtest = Student('Timmy', 'Smith', 2025)
+# newtest.printname()
+# newtest.printstudent()
