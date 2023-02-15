24af77348b4a30ed149f1903c9292e55884db332
Jack Danzey <jackdanzey@gmail.com>
2023-02-15 08:26:42 -0500

    two sum problem done with python

--- prac.py
+++ prac.py
@@ -687,3 +687,27 @@
 
 
 # print(leap(2000))
+
+# Given an array of numbers, return a new array containing just two numbers (from the original array) that add up to the number 10. If there are no two numbers that add up to 10, return false.
+
+# Specifically use nested loops to solve this exercise even though there are other approaches as well.
+
+# Input: [2, 5, 3, 1, 0, 7, 11]
+# Output: [3, 7]
+
+# Input: [1, 2, 3, 4, 5]
+# Output: false (While 1, 2, 3, and 4 altogether add up to 10, we're seeking just one pair of numbers.)
+
+# input = [2, 5, 3, 1, 0, 7, 8]
+# newArray = []
+# index = 0
+# inner = 0
+
+# for x in input:
+#     for y in input:
+#         if x != y and x + y == 10:
+#             newArray.append(x)
+# if newArray == []:
+#     newArray = False
+
+# print(newArray)
