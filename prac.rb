0b68ef9747c81fd250f0bb4614016bad19232b77
Jack Danzey <jackdanzey@gmail.com>
2023-03-01 08:18:06 -0500

    bit of the ol ruby

--- prac.rb
+++ prac.rb
@@ -937,3 +937,29 @@
 # end
 
 # combine(["a", "b", "c", "d", "e"], 1)
+
+# Given two strings of equal length, write a function that returns the number of characters that are different between the two strings.
+
+# Input: "ABCDEFG", "ABCXEOG"
+# Output: 2
+
+# Explanation: While the A, B, C, E, and G are in the same place for both strings, they have different characters in the other spaces. Since there are 2 such spaces that are different (the D and F in the first string), we return 2.
+
+# Input: "ABCDEFG", "ABCDEFG",
+# Output: 0
+
+# def compare(str1, str2)
+#   index = 0
+#   count = 0
+#   while index < str1.length
+#     if str1[index] != str2[index]
+#       count += 1
+#       index += 1
+#     else
+#       index += 1
+#     end
+#   end
+#   p count
+# end
+
+# compare("ABCDEFG", "ABCXEOG")
