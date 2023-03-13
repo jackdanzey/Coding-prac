b094ffa95694747d916d6846e350edd2f0a7f642
Jack Danzey <jackdanzey@gmail.com>
2023-03-13 08:53:45 -0400

    fibo in ruby

--- prac.rb
+++ prac.rb
@@ -963,3 +963,24 @@
 # end
 
 # compare("ABCDEFG", "ABCXEOG")
+
+# Write a function that gives the Nth number of the Fibonacci Sequence. The Fibonacci sequence begins with 0 and 1, and every number thereafter is the sum of the previous two numbers. So the sequence goes like this:
+
+# 0, 1, 1, 2, 3, 5, 8, 13, 21, 34, 55, and so on until infinity...
+
+# Input: 9
+# Output: 21 (as this is the 9th number of the Fibonacci Sequence)
+
+# def fibo(num)
+#   first = 0
+#   second = 1
+#   third = 1
+#   (num - 2).times do
+#     third = second
+#     second = first + second
+#     first = third
+#   end
+#   p second
+# end
+
+# fibo(9)
