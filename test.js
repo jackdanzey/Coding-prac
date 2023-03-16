3667d753436b8347784687adfb4b6479118b4ad5
Jack Danzey <jackdanzey@gmail.com>
2023-03-16 08:45:49 -0400

    javascript multiples function

--- test.js
+++ test.js
@@ -2427,3 +2427,19 @@
 // }
 
 // fizzbuzz(30);
+
+// If we list all the natural numbers below 10 that are multiples of 3 or 5, we get 3, 5, 6 and 9. The sum of these multiples is 23.
+
+// Find the sum of all the multiples of 3 or 5 below 1000.
+
+function multi(num) {
+  let sum = 0;
+  for (let index = 1; index < num; index++) {
+    if (index % 3 === 0 || index % 5 === 0) {
+      sum = sum + index;
+    }
+  }
+  console.log(sum);
+}
+
+multi(1000);
